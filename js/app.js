da81ac97f3485c07cd1555a6bfc480acf3aa89bc
Ailene-Hoang <aileneh2020@gmail.com>
2019-10-27 21:56:16 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.header').height($(window).height() * 0.8);
+})
+
+// function displayMenu() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+
