58e84c339a97fc4d5f6344484790d2243666d54e
Ailene-Hoang <aileneh2020@gmail.com>
2019-11-20 00:25:43 -0800

    Add social media icons and About Me content.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   $('.header').height($(window).height() * 0.8);
 })
 
-// function displayMenu() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
+function displayMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
