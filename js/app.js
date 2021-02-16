a99a8326a1de0bb9e63e609482a0e5c37ca1d308
Ailene-Hoang <aileneh2020@gmail.com>
2021-02-15 17:21:55 -0800

    Change header background

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-  $('.header').height($(window).height() * 0.6);
+  $('.header').height($(window).height() * 0.8);
 })
 
 $(document).ready(function() {
-  // Transition effect for navbar 
+  // Transition effect for navbar
   $(window).scroll(function() {
     // checks if window is scrolled more than 400px, adds/removes solid class
-    if($(this).scrollTop() > 400) { 
+    if($(this).scrollTop() > 400) {
         $('.navbar').addClass('solid');
     } else {
         $('.navbar').removeClass('solid');
     }
   })
 })
-
